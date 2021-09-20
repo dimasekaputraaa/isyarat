5719103bf796ac2e4b46e74abf14d9a613dc5ed5
dimasekaputraaa <dimasteknik018@gmail.com>
2021-09-20 17:41:17 +0800

    push

---
+++ index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <!-- navbar -->
+    <!-- <nav class="navbar">
+    <div class="container">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+        <ul class="nav justify-content-end">
+            <li class="nav-item active" active>
+                <a class="nav-link" href="#">Beranda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Bisindo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Bisi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Game</a>
+            </li>
+        </ul>
+        
+        
+    </div>
+    </nav> -->
+    
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+    <div class="container">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+        <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon justify-content-end"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+    
+    
+
+    <!-- navbar akhir -->
+    <section id="atas">
+        <div class="container">
+            <div class="row">
+                <div class="kiri col-lg-6">
+                    <span class="tek">Mari Belajar </span>
+                    <span>Bahasa Isyarat</span>
+                </div>
+                <div class="kanan col-lg-6">
+                    <div class="gambar">
+                        <img src="img/bisindo.png" class="rounded img-responsive center-block d-block mx-auto" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="konten col-8">
+                    <p>
+                        Bisindo adalah platform belajar bahasa isyarat berbasis website yang dapat diakses 
+                        diakses dimana saja dan siapa saja.
+                    </p>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        Voluptatem unde dolor architecto, dolore iusto cupiditate sapiente. 
+                        Earum, voluptates nobis sed esse vero, eligendi exercitationem tempore, 
+                        laboriosam eaque illo tempora! Sequi?
+                    </p>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        Voluptatem unde dolor architecto, dolore iusto cupiditate sapiente. 
+                        Earum, voluptates nobis sed esse vero, eligendi exercitationem tempore, 
+                        laboriosam eaque illo tempora! Sequi?
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- footer -->
+    <div class="card-footer text-muted">
+        2 days ago
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
